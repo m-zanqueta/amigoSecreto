2c418eab083464b12b40161a36f7b08d05cbce6e
Zanqueta <matheuszanqueta20@gmail.com>
2025-08-01 22:59:33 -0300

    Criando o arquivo gitignore

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ function limparCampo(){
     let campo = document.querySelector('input');
     campo.value = '';
 }
+
+function adicionarAmigo (){
+    let amigo = document.querySelector('input').value;
+    amigos.push(amigo);
+    console.log(amigos);
+    limparCampo()
+}
