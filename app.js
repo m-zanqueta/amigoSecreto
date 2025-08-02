5f32573c362e5af92c4df6e0d48f02852d3f4a1a
Zanqueta <matheuszanqueta20@gmail.com>
2025-08-01 23:14:18 -0300

    Criando a função para mudar o texto

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 let amigos = [];
 
-function limparCampo(){
-    let campo = document.querySelector('input');
+function limparCampo(tag){
+    let campo = document.querySelector(tag);
     campo.value = '';
+}
+
+function mudaTexto(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML(texto);
 }
 
 function adicionarAmigo (){
     let amigo = document.querySelector('input').value;
     amigos.push(amigo);
-    console.log(amigos);
-    limparCampo();
+    console.log(amigos); //testando a incrementação na lista
+    limparCampo('input');
 }
 
 function sortearAmigo(){
-    num = amigos.length();
-    console.log(num);
+    num = amigos.length;
+    res = Math.round(Math.random() * (num - 1));
+    console.log(amigos[res]); // vendo o amigo sorteado
 }
